7ad36832bd0eaba3d08598f251f4d1da63b1ea8a
Ivan Krastev <igkrastev@gmail.com>
2025-01-22 01:06:59 +0200

    Updated code

--- functions.py
+++ functions.py
@@ -79,8 +79,9 @@ def set_daily_goals():
     - Update the global variables workout_goal and calorie_goal.
     - Print a confirmation message.
     """
-    workout_time = int(input("How many minutes would you like to train today? Minutes: "))
-    calorie_limit = float(input("How many calories would you like to consume today? Calories: "))
+    workout_time = check_input("How many minutes would you like to train today? Minutes: ")
+    workout_time = int(workout_time)
+    calorie_limit = check_input("How many calories would you like to consume today? Calories: ")
     print(f"You have set a goal to workout {workout_time} minutes "
           f"and consume {int(calorie_limit) if calorie_limit.is_integer() else calorie_limit} calories today. We believe you can do it! 💪")
     return workout_time, calorie_limit
